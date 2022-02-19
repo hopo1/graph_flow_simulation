c7ee95f42b3a499f70820b996a23fb9613aba14d
horsky <horskdav@fit.cvut.cz>
2022-02-19 13:48:12 +0100

    Basic fix.

--- EncodeProcessDecodeBasic.py
+++ EncodeProcessDecodeBasic.py
@@ -2,6 +2,8 @@ import sonnet as snt
 import tensorflow as tf
 from graph_nets import blocks
 
+from common import NodeType
+
 
 class EncodeProcessDecode(snt.Module):
     def __init__(self, steps, learn_features, node_net_sizes, edge_net_sizes, name="EncodeProcessDecode"):
@@ -24,6 +26,13 @@ class EncodeProcessDecode(snt.Module):
         d3 = self._to_nodes(d2)
         new_speed = grp.nodes[..., :self.learn_features] + d3.nodes[..., :self.learn_features]
         return grp.replace(nodes=tf.concat([new_speed, grp.nodes[..., self.learn_features:]], 1))
+
+    def loss(self, grp, data):
+        grp = self(grp)
+        loss_mask = tf.logical_or(tf.equal(data['node_type'][:, 0], NodeType.NORMAL),
+                                  tf.equal(data['node_type'][:, 0], NodeType.OUTFLOW))
+        err = tf.reduce_sum((grp.nodes[..., :2] - data['target|velocity']) ** 2, axis=1)
+        return tf.reduce_mean(err[loss_mask])
 
     def __call__(self, grp, is_learning=False):
         for _ in range(self.steps):
